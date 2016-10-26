8458838877811268385e602f28cc0e653c5cc845
Victor Saint Guilhem <vsaintguilh@enseirb-matmeca.fr>
2016-10-26 14:44:49 -0500

    bob

--- matrixNorm.cu
+++ matrixNorm.cu
@@ -188,7 +188,7 @@ __global__ void matrixNormKernel(float * _A, float * _B, int size) {
     int bx = blockIdx.x;
     int gd = gridDim.x;
     int row;
-    _A[tx] = 0.0;
+    _A[0][0] = 0.0;
     // float mu, sigma;
     //
     // // Use of a share copy of _A and _B
